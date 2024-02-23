c09ee581fdc5556349f69839a06184540425d56e
Ujwal KC <ujwalkcsps@gmail.com>
2024-02-24 02:52:24 +0530

    ass saved

---
+++ authentication/index.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@webapp-postgres:5432/db_name'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = generate_password_hash(password)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter_by(username=username).first()
+
+    if user and check_password_hash(user.password, password):
+        return jsonify({'message': 'Login successful!'})
+    else:
+        return jsonify({'message': 'Invalid username or password.'}), 401
+
+if __name__ == '__main__':
+    app.run(debug=True)
